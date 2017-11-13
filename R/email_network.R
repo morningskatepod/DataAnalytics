4ff57e57f2c4688e1a618c685b8a2cc0e9bde5df
Douglas M. Liebe <liebe2@vt.edu>
2017-11-13 16:08:08 -0500

    bigger sample returns df of sums by users

--- R/email_network.R
+++ R/email_network.R
@@ -7,10 +7,14 @@ employee_list <-  unique(psycho$user)
 nrc_neg <- get_sentiments('nrc') %>%
   filter(sentiment == 'negative')
 
-emails <- read.csv('email_info.csv', nrows = 1000)
+emails <- read.csv('email_info.csv', nrows = 10000)
 emails$content <- as.character(emails$content)
 neg_emails <- emails %>%
-  select(user, content) %>%
+  select(user, date, content) %>%
   unnest_tokens(word, content) %>%
   inner_join(nrc_neg) %>%
   count(word, index = user, sort = TRUE)
+
+neg_users <- neg_emails %>%
+  group_by(user) %>%
+  summarise(count = sum(n)) %>% data.frame()
