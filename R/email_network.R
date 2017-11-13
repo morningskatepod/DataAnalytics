0af8c506a51749a855121b6e2d4825578aeed725
Douglas M. Liebe <liebe2@vt.edu>
2017-11-13 15:17:39 -0500

    loading this file to Github test

---
+++ R/email_network.R
@@ -0,0 +1,15 @@
+library(dplyr)
+library(tidytext)
+
+psycho <- read.csv("psychometric_info.csv")
+employee_list <-  unique(psycho$user)
+
+nrc_neg <- get_sentiments('nrc') %>%
+  filter(sentiment == 'negative')
+
+emails <- read.csv('email_info.csv', nrows = 1000)
+neg_emails <- emails %>%
+  select(user, content) %>%
+  unnest_tokens(word, content) %>%
+  inner_join(nrc_neg) %>%
+  count(word, index = user, sort = TRUE)
