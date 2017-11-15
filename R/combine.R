3cf44f0db94735919a1347ac204cbb1ae0860b9b
Douglas M. Liebe <liebe2@vt.edu>
2017-11-15 08:36:02 -0500

    change to appropriate dir in arc

--- R/combine.R
+++ R/combine.R
@@ -1,13 +1,13 @@
 library(tidyverse)
 
 # current employee from most recent employee data set
-current <- read_csv("~/data_analytics/DataSets1_9182017/Employees_info/2011-May.csv")
+current <- read_csv("~/LDAP/2011-05.csv")
 current <- current %>%
   mutate(user = stringr::str_c(stringr::str_sub(Domain, 1, 4), user_id, sep = "/"))
 
 # all employee data sites combined into single dataframe
-emp <- list.files(path = "~/data_analytics/DataSets1_9182017/Employees_info/",
+emp <- list.files(path = "~/LDAP/",
                   pattern = "*.csv", full.names = TRUE)
-emp <- do.call(rbind, lapply(emp, read_csv)) %>% 
+emp <- do.call(rbind, lapply(emp, read_csv)) %>%
   mutate(user = stringr::str_c(stringr::str_sub(Domain, 1, 4), user_id, sep = "/"))
 emp$Role <- stringr::str_replace_all(emp$Role, " ", "")
