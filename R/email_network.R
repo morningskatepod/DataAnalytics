63cd1605c0dcee8ae36ed97b92d902b960e449f6
Douglas M. Liebe <liebe2@vt.edu>
2017-11-16 07:02:51 -0500

    Split task into words by user and by email, ready for implementation

--- R/email_network.R
+++ R/email_network.R
@@ -6,15 +6,25 @@ employee_list <- read.csv('Employee_info.csv')
 nrc_neg <- get_sentiments('nrc') %>%
   filter(sentiment == 'negative')
 
-emails <- read.csv('email_info.csv', nrows = 10000)
+emails <- read.csv('email_info.csv')
 emails$content <- as.character(emails$content)
-neg_emails <- emails %>%
-  select(user, date, content) %>%
-  unnest_tokens(word, content) %>%
-  inner_join(nrc_neg) %>%
-  count(word, index = user, sort = TRUE)
 
+#Splits emails by word
+neg_emails <- emails %>%
+  select(X, user, date, content) %>%
+  unnest_tokens(word, content)
+
+#Sums neg words by user
 neg_users <- neg_emails %>%
+  inner_join(nrc_neg) %>%
+  count(word, index = user, sort = TRUE) %>%
+  group_by(index) %>%
+  summarise(count = sum(n), emails = n()) %>% data.frame()
+
+#Sums neg words by ind email
+neg_indemails <- neg_emails %>%
+  inner_join(nrc_neg) %>%
+  count(word, index = X, sort = TRUE) %>%
   group_by(index) %>%
   summarise(count = sum(n)) %>% data.frame()
 
