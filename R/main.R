4ac04910f0b337682207a43f380cc197503e8932
Douglas M. Liebe <liebe2@vt.edu>
2017-11-20 14:08:54 -0500

    fix paths

--- R/main.R
+++ R/main.R
@@ -7,12 +7,12 @@ libs <- c("readr", "stringr", "lubridate", "hms", "caret", "glmnet")
 lapply(libs, library, character.only = TRUE)
 
 # tenure_distribution <- read_csv("../data/tenure_distribution.csv")
-source("../DataAnalytics/R/tenure_dist.R")
+source("DataAnalytics/R/tenure_dist.R")
 fired <- tenure_distribution %>%
   filter(attrition == 1) %>%
   arrange(end_date)
 
-all_data <- read_csv("../data/email_info.csv")
+all_data <- read_csv("data/email_info.csv")
 
 start_date <- unique(fired$start_date)
 end_dates <- unique(fired$end_date)
@@ -24,14 +24,14 @@ for(end_date in end_dates){
   end_date <- as_date(end_date)
 
   # create directory
-  dir.create(paste0("../output/", end_date))
+  dir.create(paste0("output/", end_date))
 
   # filter dates
   emails <- all_data %>%
     filter(between(day, start_date, end_date))
 
   # source files
-  source('../DataAnalytics/R/email_network.R')
+  source('DataAnalytics/R/email_network.R')
   # source("avg_on_time.R")
   # # source("avg_usb_time.R")
   # source("revised_avg_usb_time.R")
@@ -39,7 +39,7 @@ for(end_date in end_dates){
 
   # write outputs
   write_csv(email_distribution,
-            path = paste0("../output/", end_date,
+            path = paste0("output/", end_date,
                           "/email_distribution.csv"))
 
   # garbage collection
