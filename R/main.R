2ad3e3fc29b7cc71a498f98125e49e2eb4e64cf2
Douglas M. Liebe <liebe2@vt.edu>
2017-11-28 09:45:07 -0500

    use correct var in write_csv

--- R/main.R
+++ R/main.R
@@ -40,7 +40,7 @@ for(end_date in end_dates){
 
   # write outputs
   write_csv(email_distribution,
-            file = paste0("output/", end_date, '/', end_date,
+            path = paste0("output/", end_date, '/', end_date,
                           "_email_distribution.csv"))
 
   # garbage collection
