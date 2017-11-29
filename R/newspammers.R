b6d546280de3f8f62fcfd7fc7fdeb695f7f00b5d
Douglas M. Liebe <liebe2@vt.edu>
2017-11-28 21:30:46 -0500

    delete nonunique users from list and add spam var

--- R/newspammers.R
+++ R/newspammers.R
@@ -1,9 +1,12 @@
 library(dplyr)
+library(stringr)
 # emails <- read.csv('data/email_info.csv')
 emails$content <- as.character(emails$content)
 emails$day <- format(as.POSIXct(strptime(emails$date,"%m/%d/%Y %H:%M:%S",tz="")) ,format = "%Y-%m-%d")
-
+emails$spam <- ifelse(unlist(lapply(lapply(str_match_all(emails$content, "\\S+"),unique),length))<20,1,0)
 days <- unique(emails$day)
+
+emails <- subset(emails, emails$spam == 1)
 
 newusers <- c()
 daycounts <- matrix(ncol = 2, nrow = 0)
@@ -14,5 +17,5 @@ for(d in days) {
     filter(day == d) %>%
     summarise(u = unique(user)) %>% data.frame()
   daycounts <- rbind(daycounts, c(date = d,count = sum(users[,1] %in% newusers)))
-  newusers <- c(newusers, as.character(users[,1]))
+  newusers <- unique(c(newusers, as.character(users[,1])))
   }
