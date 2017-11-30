d7bcbf84259c81c577049279e528131a857d03bd
Douglas M. Liebe <liebe2@vt.edu>
2017-11-29 20:20:14 -0500

    added fb filter

--- R/facebook.R
+++ R/facebook.R
@@ -1,11 +1,22 @@
 library(dplyr)
 
+#determine time between visiting url and next url
 webtimes <- http %>%
   mutate(time = format(as.POSIXct(strptime(date,"%m/%d/%Y %H:%M:%S",tz="")) ,format = "%H:%M:%S"))
-  select(user, url, time, date) %>%
+  mutate(day = format(as.POSIXct(strptime(date,"%m/%d/%Y %H:%M:%S",tz="")) ,format = "%Y-%m-%d"))
+  select(user, url, time, date, day) %>%
   group_by(user) %>%
   arrange(date) %>%
   mutate(time_between = (time - lead(time, 1))/60) %>%
   ungroup() %>%
   filter(time_between > 0) %>%
   data.frame()
+
+# filter out the urls on facebook
+facebook <- webtimes %>%
+  mutate(fb = sum(str_detect(url, 'facebook'))) %>%
+  filter(fb == 1, time_between < 60) %>%
+  group_by_(.dots = c("day","user")) %>%
+  summarise(total = sum(time_between)) %>% ungroup() %>%
+  group_by(user) %>%
+  summarise(median = median(total), avg = mean(total)) %>% data.frame()
