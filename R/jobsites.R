90e9cf1487df04e6c0a260f6d9b5c56387b1995d
Douglas M. Liebe <liebe2@vt.edu>
2017-12-05 11:38:12 -0500

    only count once per day

--- R/jobsites.R
+++ R/jobsites.R
@@ -21,5 +21,8 @@ http$visit <- unlist(lapply(http$url, FUN = jobuse))
 # filter out the urls on facebook
 total_visits <- http %>%
   filter(visit == TRUE) %>%
+  mutate(day = format(as.POSIXct(strptime(date,"%m/%d/%Y %H:%M:%S",tz="")) ,format = "%Y-%m-%d")) %>%
+  group_by_(.dots = c("day","user")) %>%
+  summarise(total = ifelse(sum(visit)>0, 1,0)) %>% ungroup() %>%
   group_by(user) %>%
-  summarise(sites_visited = sum(visit)) %>% data.frame()
+  summarise(days_onsite = sum(total)) %>% data.frame()
